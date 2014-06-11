c65e782e6a01c35e9b26d6eba5e5bfb0909a92f3
pam- <pam.assogba@gmail.com>
2014-06-11 18:49:48 -0400

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
   	if @user.save
   		redirect_to @user
-  		flash[:success] = "Account successfully created!"
+  		flash[:success] = "Welcome! Happy venting!"
   	else 
   		render 'new'
   	end 
