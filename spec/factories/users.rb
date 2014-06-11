ff641da171bd3a84337edef4354f8c06fc98fcd0
pam- <pam.assogba@gmail.com>
2014-06-11 13:35:57 -0400

    Finish user signup

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :user do
-    name "MyString"
-    email "MyString"
-  end
+  factory :user do 
+		name "Pamela Assogba"
+		email "pamela@example.com"
+		password "poopoo"
+		password_confirmation "poopoo"
+	end 
 end
