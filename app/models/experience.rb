48738a67f99cc03d08dc98af953fdacfe55787ac
pam- <pam.assogba@gmail.com>
2014-06-24 19:33:20 -0400

    fixes for heroku

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,7 +2,7 @@ class Experience < ActiveRecord::Base
 	belongs_to :user 
 	default_scope -> { order('created_at DESC') } #descending order from SQL
 	validates :user_id, presence: true #makes sure user_id is never nil
-	validates :content, presence: true, length: { maximum: 140 }
+	validates :content, presence: true
 	validates :content, presence: true
 	validates :title, presence: true
 	
